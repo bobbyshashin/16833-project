d6b5aee4bdc0ba3d9ceb3e1e7be9b4ac1390babe
boshenniu <bniu@andrew.cmu.edu>
2020-05-07 20:40:13 -0400

    updated LCD result analysis script

--- aloam_ws/src/A-LOAM/scripts/analysis.py
+++ aloam_ws/src/A-LOAM/scripts/analysis.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 import icp
 
-id = '06'
+id = '05_sharp'
 
 pc_list = pickle.load(open('pc'+id+'.p', 'rb'))
 print(len(pc_list))
@@ -15,14 +15,12 @@ path_list = pickle.load(open('path'+id+'.p', 'rb'))
 print(len(path_list))
 print(type(path_list[0]))
 
-fig = plt.figure(figsize=(20,10))
 x_list = []
 y_list = []
 for pose in path_list:
     x_list.append(pose.pose.position.x)
     y_list.append(pose.pose.position.y)
-# plt.scatter(x_list, y_list)
-plt.plot(x_list, y_list, '.', markersize=1)
+plt.plot(x_list, y_list, '.', markersize=2)
 
 past_key_frame = {}
 for i in range(len(pc_list)):
@@ -43,7 +41,7 @@ for i in range(len(pc_list)):
                     frame_id_ls.append(key_frame_id) 
             if len(icp_distances_ls) > 0:
                 min_distance = min(icp_distances_ls)
-                if min_distance < 1.15:
+                if min_distance < 1.15: # 0.6 for '00_corner_last'
                     closest_id = frame_id_ls[ np.argmin(icp_distances_ls) ]
                     print("input frame id: {}, closest past frame id: {}, distance: {:.4f}"
                           .format(i, closest_id, min_distance))
@@ -58,19 +56,10 @@ for i in range(len(pc_list)):
                         if pose.header.stamp > time2:
                             x2, y2 = pose.pose.position.x, pose.pose.position.y
                             break
-                    plt.plot([x1, x2],[y1, y2], 'r-', linewidth=1)
+                    plt.plot([x1, x2],[y1, y2], 'r-', linewidth=5)
 
         past_key_frame[i] = PC
 
-# plt.savefig('/home/boshenniu/Downloads/'+id+'.png', dpi=400)
 plt.show()
 
 
-# fig = plt.figure()
-# ax = fig.add_subplot(111, projection='3d')
-# ax.set_xlabel('X')
-# ax.set_ylabel('Y')
-# ax.set_zlabel('Z')
-# ax.scatter(cornerPC[:, 0], cornerPC[:, 1], cornerPC[:, 2], c='r', marker='.')
-# self.plot_flag = False
-# plt.show()
